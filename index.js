2ff6f491249040606b6107f47cea54093ac1f735
tommasovenza <tommasovenza@gmail.com>
2023-02-11 18:07:18 +0100

    added slideshow and its controls

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-// things I need
+// things I need Initially
 const addBtn = document.querySelector(".plus")
 const removeBtn = document.querySelector(".minus")
 
+function showRightImage(numberOfRightSlide) {
+  const images = document.querySelectorAll(".image-container > img")
+  console.log(images)
+  images.forEach((img) => img.classList.remove("active"))
+  images[numberOfRightSlide - 1].classList.add("active")
+}
+
+function selectActiveImage() {
+  const containerImages = document.querySelector(".image-container")
+  const images = document.querySelectorAll(".image-container > img")
+  images.forEach((img) => img.classList.remove("active"))
+
+  // get last image
+  const active = containerImages.lastElementChild
+  active.classList.add("active")
+  console.log(active)
+}
+
+function changeInputSlider(number) {
+  selectActiveImage()
+
+  const inputRange = document.getElementById("range")
+  const label = document.getElementById("label")
+  inputRange.max = number
+  //inputRange.min = 1
+  label.textContent = number
+
+  console.log(inputRange)
+
+  inputRange.addEventListener("click", (e) => {
+    const number = parseInt(e.target.value)
+    showRightImage(number)
+  })
+}
+function countImages() {
+  const images = document.querySelectorAll(".image-container > img")
+  const numOfImages = images.length
+  //console.log(numOfImages)
+  changeInputSlider(numOfImages)
+}
+
 function loadImages() {
   const fileInput = document.getElementById("fileInput")
+  //const inputRange = document.getElementById("range")
   //console.log(fileInput.files[0])
 
   fileInput.addEventListener("change", (event) => {
@@ -14,16 +56,17 @@ function loadImages() {
     //console.log(fileReader.result)
 
     fileReader.addEventListener("load", () => {
-      const imageContainer = document.querySelector("#image-container")
+      const imageContainer = document.querySelector(".image-container")
       const url = fileReader.result
       console.log(url)
       const image = document.createElement("img")
       image.src = url
-
-      console.log(image)
-      console.log(imageContainer)
+      // console.log(image)
+      // console.log(imageContainer)
       imageContainer.appendChild(image)
       //imageContainer.appendChild(image)
+
+      countImages()
     })
   })
 }
@@ -33,13 +76,18 @@ function addComponent() {
   const el = document.createElement("div")
   el.classList.add("component")
   el.innerHTML = `
-    <div id="image-container"></div>
+    <div class="input-range">
+      <label for="range" id="label">1</label>
+      <input type="range" id="range" class="range" min="1" max="100" />
+    </div>  
+    <div class="image-container"></div>
     <div class="upload">
       <input type="file" id="fileInput" />
       <button class="btn">Upload</button>
     </div>
   `
   container.appendChild(el)
+  loadImages()
 }
 function removeComponent() {
   const container = document.querySelector(".container-component")
@@ -49,12 +97,3 @@ function removeComponent() {
 // event listeners
 addBtn.addEventListener("click", addComponent)
 removeBtn.addEventListener("click", removeComponent)
-
-document.addEventListener("click", (e) => {
-  // console.log(e.target.textContent)
-  const uploadBtn = document.querySelector(".upload > .btn")
-
-  if (e.target.textContent === "Upload") {
-    loadImages()
-  }
-})
